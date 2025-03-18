98c94326be3ae6ab94a557ce36c530b68ea29a04
Christian <christianftcosta@gmail.com>
2025-03-18 16:07:22 -0300

    Update test_product.py

--- tests/controllers/test_product.py
+++ tests/controllers/test_product.py
@@ -1,102 +1,53 @@
 from typing import List
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from pydantic import UUID4
+from store.core.exceptions import NotFoundException
 
-import pytest
-from tests.factories import product_data
-from fastapi import status
+from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
+from store.usecases.product import ProductUsecase
 
-
-async def test_controller_create_should_return_success(client, products_url):
-    response = await client.post(products_url, json=product_data())
-
-    content = response.json()
-
-    del content["created_at"]
-    del content["updated_at"]
-    del content["id"]
-
-    assert response.status_code == status.HTTP_201_CREATED
-    assert content == {
-        "name": "Iphone 14 Pro Max",
-        "quantity": 10,
-        "price": "8.500",
-        "status": True,
-    }
+router = APIRouter(tags=["products"])
 
 
-async def test_controller_get_should_return_success(
-    client, products_url, product_inserted
-):
-    response = await client.get(f"{products_url}{product_inserted.id}")
-
-    content = response.json()
-
-    del content["created_at"]
-    del content["updated_at"]
-
-    assert response.status_code == status.HTTP_200_OK
-    assert content == {
-        "id": str(product_inserted.id),
-        "name": "Iphone 14 Pro Max",
-        "quantity": 10,
-        "price": "8.500",
-        "status": True,
-    }
+@router.post(path="/", status_code=status.HTTP_201_CREATED)
+async def post(
+    body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    return await usecase.create(body=body)
 
 
-async def test_controller_get_should_return_not_found(client, products_url):
-    response = await client.get(f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca")
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {
-        "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
-    }
-
-
-@pytest.mark.usefixtures("products_inserted")
-async def test_controller_query_should_return_success(client, products_url):
-    response = await client.get(products_url)
-
-    assert response.status_code == status.HTTP_200_OK
-    assert isinstance(response.json(), List)
-    assert len(response.json()) > 1
+@router.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    try:
+        return await usecase.get(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
 
-async def test_controller_patch_should_return_success(
-    client, products_url, product_inserted
-):
-    response = await client.patch(
-        f"{products_url}{product_inserted.id}", json={"price": "7.500"}
-    )
-
-    content = response.json()
-
-    del content["created_at"]
-    del content["updated_at"]
-
-    assert response.status_code == status.HTTP_200_OK
-    assert content == {
-        "id": str(product_inserted.id),
-        "name": "Iphone 14 Pro Max",
-        "quantity": 10,
-        "price": "7.500",
-        "status": True,
-    }
+@router.get(path="/", status_code=status.HTTP_200_OK)
+async def query(usecase: ProductUsecase = Depends()) -> List[ProductOut]:
+    return await usecase.query()
 
 
-async def test_controller_delete_should_return_no_content(
-    client, products_url, product_inserted
-):
-    response = await client.delete(f"{products_url}{product_inserted.id}")
+@router.patch(path="/{id}", status_code=status.HTTP_200_OK)
+async def patch(
+    id: UUID4 = Path(alias="id"),
+    body: ProductUpdate = Body(...),
+    usecase: ProductUsecase = Depends(),
+) -> ProductUpdateOut:
+    try:
+        return await usecase.update(id=id, body=body)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
-    assert response.status_code == status.HTTP_204_NO_CONTENT
 
-
-async def test_controller_delete_should_return_not_found(client, products_url):
-    response = await client.delete(
-        f"{products_url}4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
-    )
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {
-        "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
-    }
+@router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> None:
+    try:
+        await usecase.delete(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
